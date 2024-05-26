114101db6718584ebb906e45676d8f076d412f4c
Nechirvan Zibar <nechirvan.zibar@bwedu.de>
2024-05-26 13:03:20 +0200

    Chore: Generated Docstrings

--- fraud_detection_pipeline.py
+++ fraud_detection_pipeline.py
@@ -20,6 +20,30 @@ from onnxruntime import InferenceSession
 
 
 class DataProcessor:
+    """
+    A class that processes data for fraud detection pipeline.
+
+    Args:
+        data_path (str): The path to the data file.
+        test_split (float, optional): The proportion of data to be used for testing. Defaults to 0.2.
+
+    Attributes:
+        data_path (str): The path to the data file.
+        x_train (numpy.ndarray): The training data features.
+        x_test (numpy.ndarray): The testing data features.
+        y_train (numpy.ndarray): The training data labels.
+        y_test (numpy.ndarray): The testing data labels.
+        test_split (float): The proportion of data to be used for testing.
+        scaler (StandardScaler): The scaler used for data preprocessing.
+
+    Methods:
+        process_data: Loads, splits, and preprocesses the data.
+        load_data: Loads the data from the specified file path.
+        split_data: Splits the data into training and testing sets.
+        preprocess_data: Preprocesses the data using a scaler.
+
+    """
+
     def __init__(self, data_path, test_split=0.2) -> None:
         self.data_path = data_path
         self.x_train = None
@@ -30,12 +54,23 @@ class DataProcessor:
         self.scaler = StandardScaler()
 
     def process_data(self) -> tuple:
+        """
+        Loads, splits, and preprocesses the data.
+
+        Returns:
+            tuple: A tuple containing the training and testing data features and labels.
+
+        """
         self.load_data()
         self.split_data()
         self.preprocess_data()
         return self.x_train, self.x_test, self.y_train, self.y_test
 
     def load_data(self) -> None:
+        """
+        Loads the data from the specified file path.
+
+        """
         logging.info("Loading data...")
         data = pd.read_csv(self.data_path)
         self.X = data.drop("Class", axis=1)
@@ -43,6 +78,10 @@ class DataProcessor:
         logging.info("Data loaded.")
 
     def split_data(self) -> None:
+        """
+        Splits the data into training and testing sets.
+
+        """
         logging.info("Splitting data...")
         (self.x_train, self.x_test, self.y_train, self.y_test) = train_test_split(
             self.X, self.y, test_size=self.test_split, random_state=42
@@ -50,6 +89,10 @@ class DataProcessor:
         logging.info("Data splitted.")
 
     def preprocess_data(self) -> None:
+        """
+        Preprocesses the data using a scaler.
+
+        """
         logging.info("Preprocessing data...")
         self.x_train = self.scaler.fit_transform(self.x_train)
         self.x_test = self.scaler.transform(self.x_test)
@@ -57,12 +100,40 @@ class DataProcessor:
 
 
 class ModelTrainer:
+    """
+    A class for training machine learning models.
+
+    Args:
+        x_train (array-like): The input features for training.
+        y_train (array-like): The target variable for training.
+
+    Attributes:
+        model: The trained machine learning model.
+        x_train (array-like): The input features for training.
+        y_train (array-like): The target variable for training.
+
+    Methods:
+        train_model: Trains the machine learning model.
+
+    """
+
     def __init__(self, x_train, y_train) -> None:
         self.model = None
         self.x_train = x_train
         self.y_train = y_train
 
     def train_model(self, n_estimators=50, random_state=42) -> RandomForestClassifier:
+        """
+        Trains the machine learning model.
+
+        Args:
+            n_estimators (int, optional): The number of trees in the random forest. Defaults to 50.
+            random_state (int, optional): The random seed for reproducibility. Defaults to 42.
+
+        Returns:
+            RandomForestClassifier: The trained random forest classifier model.
+
+        """
         logging.info("Training model...")
         self.model = RandomForestClassifier(
             n_estimators=n_estimators, random_state=random_state
@@ -73,12 +144,31 @@ class ModelTrainer:
 
 
 class ModelEvaluator:
+    """
+    Class for evaluating and visualizing the performance of a machine learning model.
+
+    Args:
+        model (InferenceSession): The trained machine learning model.
+        x_test: The input features for testing the model.
+        y_test: The true labels for testing the model.
+
+    Attributes:
+        model (InferenceSession): The trained machine learning model.
+        x_test: The input features for testing the model.
+        y_test: The true labels for testing the model.
+    """
+
     def __init__(self, model: InferenceSession, x_test, y_test) -> None:
         self.model = model
         self.x_test = x_test
         self.y_test = y_test
 
     def evaluate_model(self) -> None:
+        """
+        Evaluates the performance of the model by calculating various metrics.
+
+        Prints the accuracy, precision, recall, F1 score, and ROC AUC score of the model.
+        """
         logging.info("Evaluating model...")
         y_pred = self.model.run(None, {"X": self.x_test})[0]
         accuracy = accuracy_score(self.y_test, y_pred)
@@ -97,6 +187,11 @@ class ModelEvaluator:
         )
 
     def visualize_confusion_matrix(self) -> None:
+        """
+        Visualizes the confusion matrix of the model's predictions.
+
+        Plots a heatmap of the confusion matrix using seaborn.
+        """
         logging.info("Visualizing confusion matrix...")
         y_pred = self.model.run(None, {"X": self.x_test})[0]
         cm = confusion_matrix(self.y_test, y_pred)
@@ -110,11 +205,41 @@ class ModelEvaluator:
         logging.info("Confusion matrix visualized.")
 
 class ModelManager:
+    """
+    A class that manages the saving, loading, and prediction of a machine learning model.
+
+    Attributes:
+        model_path (str): The path to save/load the model.
+        model (InferenceSession): The loaded model.
+
+    Methods:
+        save_model(model, x_train) -> bool: Saves the model as an ONNX file.
+        load_model() -> bool: Loads the model from the saved ONNX file.
+        get_prediction(data) -> int: Makes a prediction using the loaded model.
+        get_model() -> InferenceSession: Returns the loaded model.
+    """
+
     def __init__(self, model_path) -> None:
+        """
+        Initializes a new instance of the ModelManager class.
+
+        Args:
+            model_path (str): The path to save/load the model.
+        """
         self.model_path = model_path
         self.model = None
     
     def save_model(self, model, x_train) -> bool:
+        """
+        Saves the model as an ONNX file.
+
+        Args:
+            model: The trained machine learning model.
+            x_train: The input data used for training the model.
+
+        Returns:
+            bool: True if the model is successfully saved, False otherwise.
+        """
         logging.info("Saving model...")
         onnx_model = to_onnx(model, x_train)
         try:
@@ -127,6 +252,12 @@ class ModelManager:
         return True
     
     def load_model(self) -> bool:
+        """
+        Loads the model from the saved ONNX file.
+
+        Returns:
+            bool: True if the model is successfully loaded, False otherwise.
+        """
         logging.info("Loading model...")
         try:
             with open(self.model_path, "rb") as f:
@@ -138,6 +269,15 @@ class ModelManager:
         return True
     
     def get_prediction(self, data) -> int:
+        """
+        Makes a prediction using the loaded model.
+
+        Args:
+            data: The input data for making the prediction.
+
+        Returns:
+            int: The predicted value.
+        """
         logging.info("Predicting...")
         try:
             prediction = self.model.run(None, {"X": data})[0]
@@ -148,6 +288,12 @@ class ModelManager:
         return prediction
     
     def get_model(self) -> InferenceSession:
+        """
+        Returns the loaded model.
+
+        Returns:
+            InferenceSession: The loaded model.
+        """
         return self.model
     
 
